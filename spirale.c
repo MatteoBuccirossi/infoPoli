9ef72efa1deddf0fd81de37f098160b74d5fa783
Matteo Buccirossi <matteobuccirossi@gmail.com>
2022-09-27 12:08:47 +0200

    spirale.c : half -> n

--- spirale.c
+++ spirale.c
@@ -5,20 +5,19 @@ int main(){
 	int n;
 	printf("inserire numero (1-9): ");
 	scanf ("%d", &n);
-	
+
 	int odd = n*2 -1;
-	int half =( odd / 2 )+1;
-	
+
 	int i;
 	for(i =1; i <= odd; i++){
 		int w;
 		for(w = 1; w<=odd; w++){
-			if(abs(half - i) >= abs(half -w)){
-				printf("%d", abs(half- i) +1);
+			if(abs(n - i) >= abs(n -w)){
+				printf("%d", abs(n- i) +1);
 			}else{
-				printf("%d", abs(half- w) +1);
+				printf("%d", abs(n- w) +1);
 			}
-			
+
 			//newline
 			if(w == odd){
 				printf("\n");
